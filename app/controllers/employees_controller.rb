b5dae0ffd95843a931d492182ca9a9a0d3453808
woink <wardprice@gmail.com>
2020-08-06 16:17:30 -0400

    need to add create and add page

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,29 @@
 class EmployeesController < ApplicationController
+
+    def index
+        @employees = Employee.all
+    end
+
+    def show
+        @employee = Employee.find(params[:id])
+    end
+
+    def create
+        @employee = Employee.new(employee_params(:first_name, :last_name, :alias, :title, :office))
+        @employee.save
+        redirect_to employee_path(@employee)
+    end
+
+    def update
+        @employee = Employee.find(params[:id])
+        @employee.update(employee_params(:first_name, :last_name, :alias, :title, :office))
+        redirect_to employee_path(@employee)
+    end
+
+    private
+
+    def employee_params(*args)
+        params.require(:employee).permit(*args)
+    end
 end
+
--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,2 +1,9 @@
 class Dog < ApplicationRecord
+    has_many :employees
+
+    def owners
+        Employee.all.filter do |employee|
+            employee.id == self.id
+        end
+    end
 end
